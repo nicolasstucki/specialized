43ba5d4907c36b2dddd54fb7e64bd8f2410ae5b2
Nicolas Stucki <nicolasstucki@gmail.com>
2013-02-21 10:05:12 +0100

    Basic definition of secialized macro 1

--- components/base/src/ch/epfl/lamp/specialized/package.scala
+++ components/base/src/ch/epfl/lamp/specialized/package.scala
@@ -1,8 +1,32 @@
-package ch.epfl.lamp.specialized.test
+package ch.epfl.lamp.specialized
+
+import scala.reflect.macros.Context
+import scala.language.experimental.macros
 
 object `package` {
 
-   def specialized[T: Manifest](f: =>Unit): Unit = f
+   def specializedUnit[T](f: => Unit): Unit = macro impl_specializedUnit[T]
+
+   def impl_specializedUnit[T: c.WeakTypeTag](c: Context)(f: c.Expr[Any]): c.Expr[Unit] = {
+      import c.universe._
+      val newAST = f.tree
+      c.Expr[Unit](Block(List(printlnTree(c)(showRaw(newAST)), printlnTree(c)(show(newAST))), f.tree))
+   }
+
    
+   def specialized[T, U](f: => U): U = macro impl_specialized[T, U]
+
    
+   def impl_specialized[T: c.WeakTypeTag, U: c.WeakTypeTag](c: Context)(f: c.Expr[Any]): c.Expr[U] = {
+      import c.universe._
+
+      val newAST = f.tree
+
+      c.Expr[U](Block(List(printlnTree(c)(showRaw(newAST)), printlnTree(c)(show(newAST))), f.tree))
+   }
+
+   private def printlnTree(c: Context)(str: String) = {
+      import c.universe._
+      Apply(Ident(newTermName("println")), List(Literal(Constant(str))))
+   }
 }
--- components/test/src/ch/epfl/lamp/specialized/test/Test.scala
+++ components/test/src/ch/epfl/lamp/specialized/test/Test.scala
@@ -1,61 +1,87 @@
 package ch.epfl.lamp.specialized.test
 
 import ch.epfl.lamp.specialized._
+import java.util.Arrays
 
 object Test extends App {
 
-   specialized {
-      println("Pjkfsajflo")
-   }
+   val v = new C[Int]
+   v.arr(0) = 1
+   v.arr(1) = 2
+   v.arr(2) = 3
+   println("Test expr 0")
+   println(v.testExpr0)
+   println
+   
+   println("Test expr 1")
+   println(v.testExpr1)
+   println
+   
+   println("Test expr 2")
+   println(v.testExpr2)
+   println
+   
+   //println( v.testExpr3 )
 
    class C[T: Manifest] {
-     val arr = new Array[T](1)
+      val arr = new Array[T](3)
 
-     // original:
-     def testExpr1 = {
-       specialized[T] {
-         arr.length
-       } + 1
-     }
+      // original:
+      def testExpr0 = {
+         specializedUnit[T] {
+            val tup = (arr(0),arr(2)) 
+            println("testExpr0 message: " + tup )
+         }
+      }
 
-    def testExpr2 = {
-       arr(0) = specialized[T] {
-         arr(0)
-       }
-     }
+      def testExpr1 = {
+         specialized[T,Int] {
+            arr.length
+         } + specialized/*[T,Int]*/ {
+            arr.length
+         } + specialized/*[T]*/ {
+            arr.length
+         } + 1
+      }
 
-     def testExpr3 = {
-       specialized[T] {
-         List(arr(0))
-       } match {
-         case List(x: T) => println(x)
-         case _ => ???
-       }
-     }
+      def testExpr2 = {
+         arr(0) = specialized[T,T] {
+            arr(0)
+         }
+      }
 
-     // blind
-     def testExpr1unroll1 = {
-       arr.length + 1
-     }
+      def testExpr3 = {
+         specialized[T,Any] {
+            List(arr(0))
+         } match {
+            case List(x: T) => println(x)
+            case _          => ???
+         }
+      }
 
-     def testExpr2unroll1 = {
-       arr(0) = arr(0)
-     }
+      // blind
+      def testExpr1unroll1 = {
+         arr.length + 1
+      }
 
-     def testExpr3unroll1 = {
-       List(arr(0) match {
-         case List(x: T) => println(x)
-         case _ => ???
-       }
-     }
+      def testExpr2unroll1 = {
+         arr(0) = arr(0)
+      }
 
-     // with manifests
-     def testExpr2unroll2 = {
-       arr(0) =
-         if (manifest[T] == manifest[Int])
-           arr.asInstanceOf[Array[Int]](0).asInstanceOf[T]
-         else
-           ???
-     }
+      def testExpr3unroll1 = {
+         List(arr(0)) match {
+            case List(x: T) => println(x)
+            case _          => ???
+         }
+      }
+
+      //     // with manifests
+      //     def testExpr2unroll2 = {
+      //       arr(0) =
+      //         if (manifest[T] == manifest[Int])
+      //           arr.asInstanceOf[Array[Int]](0).asInstanceOf[T]
+      //         else
+      //           ???
+      //     }
    }
 }
